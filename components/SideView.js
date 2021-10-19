29c24cc3013820b8693d666fc6a7e8f11628ecc8
ashwinhubli <66745007+ashwinhubli@users.noreply.github.com>
2021-10-19 18:50:45 +0530

    Add files via upload

---
+++ components/SideView.js
@@ -0,0 +1,43 @@
+AFRAME.registerComponent("place-side-view",{
+ createPlaceThumbnail:function(position,id){
+  const entityEl = document.createElement("a-entity")
+  entityEl.setAttribute("visible",true)
+  entityEl.setAttribute("id",'place-${id}')
+  entityEl.setAttribute("geometry",{
+     primitive:"circle",
+     radius:2.5 
+  })
+  entityEl.setAttribute("material",{
+   src:"./assets/helicopter.png",
+   opacity:0.9 
+  })
+  entityEl.setAttribute("position",position)
+  entityEl.setAttribute("cursor-listener",{})
+  return entityEl   
+ },
+ createPlaces:function(){
+  const sideViewContainer = document.querySelector("#side-view-container")
+  let previousXPosition = -150
+  let previousYPosition = 30
+  
+  for(var i=1;i<=4;i++){
+   const position = {x:(previousXPosition+=50),y:(previousYPosition+=2),z:(-40)}
+   const entityEl = this.createPlaceThumbnail(position,i)
+   sideViewContainer.appendChild(entityEl)
+  }  
+ }, 
+ tick:function(){
+  const placesContainer = document.querySelector("#places-container") 
+  const {state} = placesContainer.getAttribute("tour")
+  if(state==="view"||state==="change-view"){
+   this.el.setAttribute("visible",true) 
+  }
+  else{
+   this.el.setAttribute("visible",false)   
+  }
+
+ },
+ init:function(){
+  this.createPlaces()
+ } 
+})
